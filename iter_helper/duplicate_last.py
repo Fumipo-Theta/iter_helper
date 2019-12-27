7b292d8974422a73cd0d0fdc77048532f359d112
twined quartz <fmorisato@gmail.com>
2019-12-27 09:39:11 +0900

    add type

--- iter_helper/duplicate_last.py
+++ iter_helper/duplicate_last.py
@@ -1,9 +1,14 @@
-class DuplicateLast:
-    def __init__(self, *arg):
+from typing import Generic, List, TypeVar
+
+T = TypeVar("T")
+
+
+class DuplicateLast(Generic[T]):
+    def __init__(self, *arg: T):
         self.args = arg
 
-    def __len__(self):
+    def __len__(self)->int:
         return len(self.args)
 
-    def __getitem__(self, n):
+    def __getitem__(self, n: int)->T:
         return self.args[n] if len(self) > n else self.args[-1]
